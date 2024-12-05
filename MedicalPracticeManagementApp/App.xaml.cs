373da643cd2068a206c770bdf37c8111adad1224
Visvakarm <Markmedwid1@gmail.com>
2024-12-05 13:46:58 -0500

    h-m

---
+++ MedicalPracticeManagementApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicalPracticeManagementApp_
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MedicalPracticeManagementApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MedicalPracticeManagementApp_
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
